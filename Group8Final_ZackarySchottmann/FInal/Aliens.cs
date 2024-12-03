725ce5a47de98c1e153b9fb678cda62b71634ae3
zackschottmann <zschottmann5144@conestogac.on.ca>
2024-12-03 14:37:49 -0500

    last update of my final code

---
+++ Group8Final_ZackarySchottmann/FInal/Aliens.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FInal
+{
+
+
+    public class Alien
+    {
+        public Vector2 Position { get;  set; }
+        public Texture2D Texture { get;  set; }
+        public bool IsAlive { get; set; }
+        public Rectangle BoundingBox => new Rectangle((int)Position.X, (int)Position.Y, Texture.Width, Texture.Height);
+
+        public Vector2 Velocity; 
+        private int ScreenWidth;
+
+        public Alien(Texture2D texture, Vector2 startPosition, Vector2 velocity, int screenWidth)
+        {
+            Texture = texture;
+            Position = startPosition;
+            Velocity = velocity;
+            ScreenWidth = screenWidth;
+            IsAlive = true;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (!IsAlive) return;
+
+
+            Position += Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (Position.X <= 0 || Position.X + Texture.Width >= ScreenWidth)
+            {
+                Velocity.X = -Velocity.X;
+                Position += Velocity; 
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (IsAlive)
+            {
+                spriteBatch.Draw(Texture, Position, Color.White);
+            }
+        }
+    }
+}
