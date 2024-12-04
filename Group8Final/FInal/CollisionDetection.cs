c6cbe29a08d70544c5c19576e711b0fe0f695746
zackschottmann <zschottmann5144@conestogac.on.ca>
2024-12-04 00:15:21 -0500

    Final Project

---
+++ Group8Final/FInal/CollisionDetection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace FInal
+{
+    internal class CollisionDetection
+    {
+        public static bool CheckCollision(Rectangle rect1, Rectangle rect2)
+        {
+            return rect1.Intersects(rect2);
+        }
+    }
+}
---
+++ Group8Final/FInal/Physics.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FInal
+{
+
+    public enum AnimationState
+    {
+        Idle,      
+        Moving,    
+        Firing     
+    }
+
+    public class Player
+    {
+        public Vector2 Position { get; set; }
+        public Rectangle BoundingBox { get; private set; }
+        public SpriteEffects Facing { get; set; } 
+        public bool IsFiring { get; set; } 
+        public AnimationState AnimationState { get; set; } 
+
+        private Texture2D _playerSprite; 
+
+
+        public Player(Texture2D playerSprite, Vector2 position)
+        {
+            _playerSprite = playerSprite;
+            Position = position;
+
+
+            BoundingBox = new Rectangle((int)Position.X, (int)Position.Y, _playerSprite.Width, _playerSprite.Height);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            BoundingBox = new Rectangle((int)Position.X, (int)Position.Y, _playerSprite.Width, _playerSprite.Height);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                _playerSprite,
+                Position,
+                null,
+                Color.White,
+                0f,
+                Vector2.Zero,
+                1f,
+                Facing,
+                0f
+            );
+        }
+
+    }
+}
